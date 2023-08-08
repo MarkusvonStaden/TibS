b71d96d1979515b915be08063eb08e5b60469d34
Markus von Staden <markus@von-staden.net>
2023-08-08 19:08:04 +0200

    init

--- main/main.c
+++ main/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-#include "bme.h"
-#include "bme280.h"
+// #include "bme.h"
+// #include "bme280.h"
+#include "driver/gpio.h"
 #include "driver/i2c.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "esp_timer.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #define I2C_MASTER_NUM        I2C_NUM_0
 #define I2C_MASTER_SDA_IO     6
@@ -12,7 +16,17 @@
 #define I2C_MASTER_FREQ_HZ    100000
 #define I2C_MASTER_TIMEOUT_MS 1000
 
-static const char *TAG = "MAIN";
+#define PIN_OUTPUT 5
+#define PIN_INPUT  4
+
+static const char* TAG = "MAIN";
+
+static int64_t start_time;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg) {
+    int64_t end_time = esp_timer_get_time();
+    ESP_LOGI(TAG, "Pin %d is HIGH. Time taken: %lld microseconds.", PIN_INPUT, end_time - start_time);
+}
 
 static esp_err_t i2c_master_init() {
     i2c_config_t i2c_config = {
@@ -26,18 +40,37 @@ static esp_err_t i2c_master_init() {
     return i2c_driver_install(I2C_MASTER_NUM, i2c_config.mode, 0, 0, 0);
 }
 
-void app_main(void) {
-    int8_t rslt;
-    i2c_master_init();
-    BME_init_wrapper();
+static void gpio_configuration(void) {
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = 1ULL << PIN_OUTPUT;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
 
-    struct bme280_data comp_data;
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    io_conf.pin_bit_mask = 1ULL << PIN_INPUT;
+    io_conf.mode = GPIO_MODE_INPUT;
+    gpio_config(&io_conf);
+
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(PIN_INPUT, gpio_isr_handler, NULL);
+}
+
+void app_main(void) {
+    gpio_configuration();
+    gpio_set_level(PIN_OUTPUT, 1);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 
     while (1) {
-        rslt = BME_force_read(&comp_data);
-        ESP_LOGD(TAG, "Force read result: %d", rslt);
+        gpio_set_level(PIN_OUTPUT, 0);
+        vTaskDelay(100);
+        gpio_set_level(PIN_OUTPUT, 1);
+        ESP_LOGI(TAG, "Pin %d is set to HIGH.", PIN_OUTPUT);
 
-        ESP_LOGI(TAG, "Temperature: %f degC, Pressure: %f Pa, Humidity: %f", comp_data.temperature, comp_data.pressure, comp_data.humidity);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        start_time = esp_timer_get_time();
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
