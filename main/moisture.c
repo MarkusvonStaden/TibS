0b0338611a93f980e340bf95e1148d8182371f9a
Markus von Staden <markus@von-staden.net>
2023-08-15 16:48:14 +0200

    cleanup complete

--- main/moisture.c
+++ main/moisture.c
@@ -1,6 +1,11 @@
 #include "driver/adc.h"
 #include "esp_log.h"
 
+// Defining constants for clarity
+#define ADC_MAX_VALUE         4095.0
+#define PERCENTAGE_MULTIPLIER 100.0
+
+// Tag for logging
 #define TAG "MOISTURE"
 
 void moisture_init(void) {
@@ -9,5 +14,5 @@ void moisture_init(void) {
 }
 
 void moisture_read(double* moisture) {
-    *moisture = (adc1_get_raw(ADC1_CHANNEL_0)) / 4095.0 * 100.0;
+    *moisture = (adc1_get_raw(ADC1_CHANNEL_0) / ADC_MAX_VALUE) * PERCENTAGE_MULTIPLIER;
 }
