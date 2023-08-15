33bbc78aefe98ef8ee29ff7f2ebcad63aa5cfa42
Markus von Staden <markus@von-staden.net>
2023-08-15 16:30:53 +0200

    cleanup main

--- main/main.c
+++ main/main.c
@@ -12,6 +12,7 @@
 #include "veml.h"
 #include "wifi.h"
 
+// Constants and Macros
 static const char *TAG = "MAIN";
 
 #define I2C_MASTER_NUM        I2C_NUM_0
@@ -20,10 +21,10 @@ static const char *TAG = "MAIN";
 #define I2C_MASTER_FREQ_HZ    100000
 #define I2C_MASTER_TIMEOUT_MS 1000
 
-#define LED_GPIO 3
-
+#define LED_GPIO           3
 #define SLEEP_TIME_SECONDS 60
 
+// Function to initialize I2C master
 static esp_err_t i2c_master_init() {
     i2c_config_t i2c_config = {
         .mode = I2C_MODE_MASTER,
@@ -36,47 +37,43 @@ static esp_err_t i2c_master_init() {
     return i2c_driver_install(I2C_MASTER_NUM, i2c_config.mode, 0, 0, 0);
 }
 
+// Function to load limits
 esp_err_t load_limits(Limits *limits) {
-    // Öffne den NVS-Handle
+    // Open the NVS handle
     nvs_handle_t my_handle;
     esp_err_t    err = nvs_open("storage", NVS_READONLY, &my_handle);
     if (err != ESP_OK) {
         return err;
     }
 
-    // Laden Sie den Struct
+    // Load the struct
     size_t required_size = sizeof(Limits);
     err = nvs_get_blob(my_handle, "limits", limits, &required_size);
-    if (err != ESP_OK) {
-        return err;
-    }
 
-    // Schließen
+    // Close the handle
     nvs_close(my_handle);
-    return ESP_OK;
+    return err;
 }
 
+// Function to evaluate limits
 void evaluate_limits(Limits *limits, double moisture, double temperature, double humidity, double pressure, double white, double visible) {
-    int i = 0;
-    if (moisture < limits->moisture.min || moisture > limits->moisture.max) {
-        i = 1;
-    } else if (temperature < limits->temperature.min || temperature > limits->temperature.max) {
-        i = 1;
-    } else if (humidity < limits->humidity.min || humidity > limits->humidity.max) {
-        i = 1;
-    } else if (pressure > limits->pressure.max || pressure < limits->pressure.min) {
-        i = 1;
-    } else if (white > limits->white.max || white < limits->white.min) {
-        i = 1;
-    } else if (visible > limits->visible.max || visible < limits->visible.min) {
-        i = 1;
-    }
-    gpio_set_level(LED_GPIO, i);
+    int alert = 0;
+
+    if (moisture < limits->moisture.min || moisture > limits->moisture.max) alert = 1;
+    if (temperature < limits->temperature.min || temperature > limits->temperature.max) alert = 1;
+    if (humidity < limits->humidity.min || humidity > limits->humidity.max) alert = 1;
+    if (pressure < limits->pressure.min || pressure > limits->pressure.max) alert = 1;
+    if (white < limits->white.min || white > limits->white.max) alert = 1;
+    if (visible < limits->visible.min || visible > limits->visible.max) alert = 1;
+
+    gpio_set_level(LED_GPIO, alert);
 }
 
+// Main application function
 void app_main(void) {
     Limits limits;
-    // init WiFi
+
+    // Initialize WiFi
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
@@ -85,27 +82,34 @@ void app_main(void) {
     ESP_ERROR_CHECK(ret);
     wifi_init_sta();
 
-    // init I2C
+    // Initialize I2C
     i2c_master_init();
     BME_init_wrapper();
 
+    // Initialize other components
     VEML_init();
     moisture_init();
     gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
 
     double moisture = 50, white, visible, temperature, humidity, pressure;
+
     while (1) {
-        ESP_LOGI(TAG, "UPDATING FKING SCESS");
+        ESP_LOGI(TAG, "Updating sensor data");
         BME_force_read(&temperature, &pressure, &humidity);
         gpio_set_level(LED_GPIO, 0);
         VEML_read(&white, &visible);
         moisture_read(&moisture);
+
         send_data(&moisture, &temperature, &humidity, &pressure, &white, &visible);
         ESP_ERROR_CHECK_WITHOUT_ABORT(load_limits(&limits));
+
         evaluate_limits(&limits, moisture, temperature, humidity, pressure, white, visible);
+
         vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
+
+    // Sleeping code (currently commented out)
+    // vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // esp_sleep_enable_timer_wakeup(SLEEP_TIME_SECONDS * 1000000LL);
+    // esp_deep_sleep_start();
 }
-// vTaskDelay(1000 / portTICK_PERIOD_MS);
-// esp_sleep_enable_timer_wakeup(SLEEP_TIME_SECONDS * 1000000LL);
-// esp_deep_sleep_start();
